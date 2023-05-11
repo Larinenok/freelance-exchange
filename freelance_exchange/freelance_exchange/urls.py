2bc86a0e487f06da0d93921b900c1fb5ddb068a9
Larinenok <olegins03@gmail.com>
2023-05-11 10:57:22 +0700

    Fix api

--- freelance_exchange/freelance_exchange/urls.py
+++ freelance_exchange/freelance_exchange/urls.py
@@ -41,7 +41,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('docs/', schema_view),
     path('', home_view),
     # path('api-token-auth/', views.obtain_auth_token),
     path('admin/', admin.site.urls),
