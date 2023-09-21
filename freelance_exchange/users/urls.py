db3460a653d74d81be28829dfc746d272ab7cf1d
Larinenok <olegins03@gmail.com>
2023-09-21 11:39:30 +0700

    Fix login paths

--- freelance_exchange/users/urls.py
+++ freelance_exchange/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     # path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('register/', RegisterView.as_view(), name='auth_register'),
-    path('login/', signin, name='login'),
-    path('register/', signup, name='register'),
-    path('refresh/', signin, name='refresh'),
+    path('login/', signin, name='login_token'),
+    path('register/', signup, name='register_token'),
+    path('refresh/', signin, name='refresh_token'),
 ]
