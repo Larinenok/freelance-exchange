dd59972d4ff3864003a77a48e15ea09d8ad4f4ef
Larinenok <olegins03@gmail.com>
2024-04-05 12:15:10 +0700

    Fix static path

--- freelance_exchange/freelance_exchange/settings.py
+++ freelance_exchange/freelance_exchange/settings.py
@@ -213,7 +213,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'https://um-stud.online/static/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
