e2f064f583871da6d2e80f8aa5da9dc146e6b46a
Larinenok <olegins03@gmail.com>
2024-04-11 14:40:10 +0700

    Clear stars/views.py

--- freelance_exchange/stars/views.py
+++ freelance_exchange/stars/views.py
@@ -1,14 +1,10 @@
-# from django.shortcuts import get_object_or_404
-# from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView, ListAPIView, RetrieveDestroyAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from users.models import *
 from ads.models import *
 from stars.models import *
 from drf_yasg.utils import swagger_auto_schema
-# from drf_yasg import openapi
 from rest_framework import permissions
 
 from .models import *
@@ -46,100 +42,3 @@ class StarChangeAPIView(GenericAPIView):
             user.save()
             return Response({'message': f'Отзыв об пользователе ({request.data.get("username")}) успешно удален'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# @swagger_auto_schema(method='get', operation_description='Get all stars')
-# @api_view(['GET'])
-# def get_all_users_stars(request):
-#     users = CustomUser.objects.all()
-#     stars = []
-#
-#     for user in users:
-#         stars.append({
-#             'username': user.slug,
-#             'stars': StarsJson.parse(user.stars_freelancer)
-#         })
-#
-#     return Response(stars)
-#
-#
-# @swagger_auto_schema(method='get', manual_parameters=[
-#     openapi.Parameter('username', openapi.IN_PATH, description='Whose review are we checking', type=openapi.TYPE_STRING, required=True)
-# ])
-# @api_view(['GET'])
-# def get_user_stars(request, username):
-#     try:
-#         user = get_object_or_404(CustomUser, slug=username)
-#
-#         return Response(StarsJson.parse(user.stars_freelancer))
-#     except:
-#         return Response({'error': 'Username not found'}, status=status.HTTP_404_NOT_FOUND)
-#
-#
-# @swagger_auto_schema(method='put', manual_parameters=[
-#     openapi.Parameter('whose', openapi.IN_QUERY, description='Whose review are we deleting', type=openapi.TYPE_STRING, required=True),
-#     openapi.Parameter('value', openapi.IN_QUERY, description='Number of stars (from 0 to 5)', type=openapi.TYPE_INTEGER, required=True)
-# ])
-# @api_view(['PUT'])
-# def set_user_stars(request, username):
-#     user_by_token = check_token(request)
-#     if not user_by_token:
-#         return Response({'error': 'Non authorized'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#     try:
-#         whose = str(request.data.get('whose'))
-#         value = int(request.data.get('value'))
-#     except:
-#         try:
-#             whose = str(request.query_params.get('whose'))
-#             value = int(request.query_params.get('value'))
-#         except:
-#             return Response({'error': 'Require "whose" and "value" parameters'}, status=status.HTTP_404_NOT_FOUND)
-#
-#     try:
-#         user = get_object_or_404(CustomUser, slug=str(username))
-#
-#         if user != user_by_token:
-#             return Response({'error': 'You can only change your reviews'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#         stars = json.dumps(StarsJson.add_star(user.stars_freelancer, username=whose, value=value))
-#         user.stars_freelancer = stars
-#         user.save()
-#
-#         return Response({'result': stars})
-#     except:
-#         return Response({'error': 'Username not found'}, status=status.HTTP_404_NOT_FOUND)
-#
-#
-# @swagger_auto_schema(method='delete', manual_parameters=[
-#     openapi.Parameter('whose', openapi.IN_QUERY, description='Whose review are we deleting', type=openapi.TYPE_STRING)
-# ])
-# @api_view(['DELETE'])
-# def delete_user_stars(request, username):
-#     user_by_token = check_token(request)
-#     if not user_by_token:
-#         return Response({'error': 'Non authorized'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#     try:
-#         whose = str(request.data.get('whose'))
-#         if whose == 'None':
-#             raise Exception('')
-#     except:
-#         try:
-#             whose = str(request.query_params.get('whose'))
-#         except:
-#             return Response({'error': 'Require "whose" parameter'}, status=status.HTTP_404_NOT_FOUND)
-#
-#     try:
-#         user = get_object_or_404(CustomUser, slug=username)
-#
-#         if user != user_by_token:
-#             return Response({'error': 'You can only change your reviews'}, status=status.HTTP_401_UNAUTHORIZED)
-#
-#         stars = json.dumps(StarsJson.remove_star(user.stars_freelancer, username=whose))
-#         user.stars_freelancer = stars
-#         user.save()
-#
-#         return Response({'result': stars})
-#     except:
-#         return Response({'error': 'Non-correct data'}, status=status.HTTP_404_NOT_FOUND)
