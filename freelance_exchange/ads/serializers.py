7366704787277d514584d087a6b532f20d14abe2
Vsevolo:d <sevan_lunev666@mail.ru>
2024-09-19 11:36:09 +0700

    full ad code refactor

--- freelance_exchange/ads/serializers.py
+++ freelance_exchange/ads/serializers.py
@@ -1,32 +1,50 @@
 from django.conf import settings
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth.password_validation import validate_password
 from pytils.translit import slugify
-
-from .models import *
+from .models import Ad, AdFile, AdResponse
 
 
 class AdSerializer(serializers.ModelSerializer):
+    files = serializers.PrimaryKeyRelatedField(queryset=AdFile.objects.all(), many=True, required=False)
+
     class Meta:
         model = Ad
-        fields = ('id', 'author', 'title', 'description', 'category', 'type', 'budget', 'pub_date', 'contact_info')
-        def create(self, validated_data):
-            ad = Ad.objects.create(
-                author=validated_data['author'],
-                title=validated_data['title'],
-                id=validated_data['id'],
-                slug=slugify(validated_data['username']),
-                description=validated_data['description'],
-                category=validated_data['category'],
-                type=validated_data['type'],
-                budget=validated_data['budget'],
-                contact_info=validated_data['contact_info'],
-            )
+        fields = (
+            'id', 'title', 'description', 'category', 'type',
+            'budget', 'contact_info', 'status', 'files'
+        )
+        read_only_fields = (
+            'author', 'pub_date', 'executor'
+        )
+
+    def create(self, validated_data):
+        files = validated_data.pop('files', [])
+        ad = Ad.objects.create(**validated_data, slug=slugify(validated_data['title']))
+        for file in files:
+            ad.files.add(file)
+        return ad
+
+    def update(self, instance, validated_data):
+        files = validated_data.pop('files', [])
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.category = validated_data.get('category', instance.category)
+        instance.type = validated_data.get('type', instance.type)
+        instance.budget = validated_data.get('budget', instance.budget)
+        instance.contact_info = validated_data.get('contact_info', instance.contact_info)
+        instance.save()
+
+        # Update files
+        if files:
+            instance.files.clear()
+            for file in files:
+                instance.files.add(file)
+
+        return instance
 
 
-class AdFileSerializer(serializers.ModelSerializer):
+class AdFileUploadSerializer(serializers.ModelSerializer):
     files = serializers.ListField(child=serializers.FileField(), required=True)
 
     class Meta:
@@ -51,3 +69,15 @@ class AdFileSerializer(serializers.ModelSerializer):
                 ad_file.save()
                 instance.append(ad_file)
         return instance
+
+
+class AdResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdResponse
+        fields = ('id', 'ad', 'responder', 'response_comment')
+
+
+class AdFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AdFile
+        fields = ('id', 'file')
--- freelance_exchange/ads/urls.py
+++ freelance_exchange/ads/urls.py
@@ -1,24 +1,32 @@
 from django.urls import path
-
-from .views import *
-
+from .views import (
+    AdListCreateView,
+    AdDetailView,
+    AdFileUploadView,
+    AdFileListView,
+    AdFileDeleteView,
+    AdResponseView,
+    AdExecutorView,
+    UserAdsView,
+    UserClosedAdsView,
+    AdsInProgressView,
+    CloseAdView,
+    # DeleteAdView,
+    GetResponsesView
+)
 
 urlpatterns = [
-    path('', all_ads),
-    path('create/', create_ad),
-    path('edit/', edit_ad),
-    path('delete/', delete_ad),
-    path('get/', api_ad_view),
-    path('get_responses/', get_responses),
-    path('set_executor/', set_executor),
-    path('adsview/<int:id>/', ad_view),
-    path('send_response/', response_ad),
-    path('ad_list/', get_ads),
-    path('upload_files/', add_file_to_ad),
-    path('file_list/', list_files_for_ad),
-    path('file_delete/', delete_file_from_ad),
-    path('close/', close_ad),
-    path('my_orders/', my_ads),
-    path('orders_completed_by_me', my_closed_ads),
-    path('orders_im_completing', ads_im_completing)
+    path('ads/', AdListCreateView.as_view(), name='ad-list'),
+    path('ads/<int:pk>/', AdDetailView.as_view(), name='ad-detail'),
+    path('ads/files/upload/', AdFileUploadView.as_view(), name='ad-file-upload'),
+    path('ads/files/', AdFileListView.as_view(), name='ad-file-list'),
+    path('ads/files/delete/', AdFileDeleteView.as_view(), name='ad-file-delete'),
+    path('ads/response/', AdResponseView.as_view(), name='ad-response'),
+    path('ads/executor/', AdExecutorView.as_view(), name='ad-executor'),
+    path('users/ads/', UserAdsView.as_view(), name='user-ads'),
+    path('users/ads/closed/', UserClosedAdsView.as_view(), name='user-closed-ads'),
+    path('users/ads/in-progress/', AdsInProgressView.as_view(), name='ads-in-progress'),
+    path('ads/close/', CloseAdView.as_view(), name='close-ad'),
+    # path('ads/delete/', DeleteAdView.as_view(), name='delete-ad'),
+    path('ads/responses/', GetResponsesView.as_view(), name='get-responses'),
 ]
