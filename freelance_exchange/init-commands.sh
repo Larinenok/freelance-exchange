48488dcec620e517c32b766dd8f8096b6b00392d
OfficialCrak <chirjeveg@gmail.com>
2024-09-26 11:30:00 +0700

    refactor: add command python3 manage.py makemigrations --noinput by file init-commands.sh

--- freelance_exchange/init-commands.sh
+++ freelance_exchange/init-commands.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py initadmin
 uwsgi --ini /etc/uwsgi.ini
